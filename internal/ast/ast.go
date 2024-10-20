58e63e3cee71a085bbbeaa90be36193479d9bf21
Deyan Delchev <deyandelchev03@gmail.com>
2024-10-20 13:38:47 +0300

    first impl for parser

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,11 +1,28 @@
 package ast
 
 import (
+	"log"
+
 	"github.com/lastvoidtemplar/json_formatter/internal/token"
 )
 
+type NodeType byte
+
+const (
+	NULL NodeType = iota
+	BOOL
+	NUMBER
+	STRING
+	UNDEFINED
+
+	ARRAY
+
+	KEYVAL
+	OBJECT
+)
+
 type Node interface {
-	String(tabs string) string
+	NodeType() NodeType
 }
 
 type LeafNode interface {
@@ -14,51 +31,56 @@ type LeafNode interface {
 }
 
 type NullNode struct {
+	Type  NodeType
 	Token token.Token
 }
 
-func (node *NullNode) String(tabs string) string {
-	return tabs + node.Token.Literal
+func (node *NullNode) NodeType() NodeType {
+	return node.Type
 }
 
 func (node *NullNode) leatNode() {}
 
 type BoolNode struct {
+	Type  NodeType
 	Token token.Token
 }
 
-func (node *BoolNode) String(tabs string) string {
-	return tabs + node.Token.Literal
+func (node *BoolNode) NodeType() NodeType {
+	return node.Type
 }
 
 func (node *BoolNode) leatNode() {}
 
 type NumberNode struct {
+	Type  NodeType
 	Token token.Token
 }
 
-func (node *NumberNode) String(tabs string) string {
-	return tabs + node.Token.Literal
+func (node *NumberNode) NodeType() NodeType {
+	return node.Type
 }
 
 func (node *NumberNode) leatNode() {}
 
 type StringNode struct {
+	Type  NodeType
 	Token token.Token
 }
 
-func (node *StringNode) String(tabs string) string {
-	return tabs + node.Token.Literal
+func (node *StringNode) NodeType() NodeType {
+	return node.Type
 }
 
 func (node *StringNode) leatNode() {}
 
 type UndefinedNode struct {
+	Type  NodeType
 	Token token.Token
 }
 
-func (node *UndefinedNode) String(tabs string) string {
-	return tabs + node.Token.Literal
+func (node *UndefinedNode) NodeType() NodeType {
+	return node.Type
 }
 
 func (node *UndefinedNode) leatNode() {}
@@ -66,35 +88,51 @@ func (node *UndefinedNode) leatNode() {}
 func NewLeafNode(tok token.Token) LeafNode {
 	switch tok.Type {
 	case token.NULL:
-		return &NullNode{Token: tok}
+		return &NullNode{Type: NULL, Token: tok}
 	case token.TRUE, token.FALSE:
-		return &BoolNode{Token: tok}
+		return &BoolNode{Type: BOOL, Token: tok}
 	case token.NUMBER_LITERAL:
-		return &NumberNode{Token: tok}
+		return &NumberNode{Type: NUMBER, Token: tok}
 	case token.STRING_LITERAL:
-		return &StringNode{Token: tok}
-	case token.UNDEFINED:
-		return &UndefinedNode{Token: tok}
+		return &StringNode{Type: STRING, Token: tok}
 	default:
 		return nil
 	}
 }
 
 type ArrayNode struct {
+	Type  NodeType
 	Nodes []Node
 }
 
-func (array *ArrayNode) String(tabs string) string {
-	return "[]"
+func (array *ArrayNode) NodeType() NodeType {
+	return array.Type
+}
+
+func (array *ArrayNode) Add(node Node) {
+	if node == nil {
+		log.Println("Tried to add nil node to array")
+		return
+	}
+
+	array.Nodes = append(array.Nodes, node)
+}
+
+func NewArrayNode() *ArrayNode {
+	return &ArrayNode{
+		Type:  ARRAY,
+		Nodes: make([]Node, 0),
+	}
 }
 
 type KeyValNode struct {
-	Key token.Token
-	Val Node
+	Type NodeType
+	Key  token.Token
+	Val  Node
 }
 
-func (node *KeyValNode) String(tabs string) string {
-	return ":"
+func (node *KeyValNode) NodeType() NodeType {
+	return node.Type
 }
 
 func NewKeyVal(key token.Token, val Node) *KeyValNode {
@@ -102,20 +140,42 @@ func NewKeyVal(key token.Token, val Node) *KeyValNode {
 		return nil
 	}
 
-	if key.Type != token.STRING_LITERAL && key.Type != token.UNDEFINED {
-		return nil
-	}
-
 	return &KeyValNode{
-		Key: key,
-		Val: val,
+		Type: KEYVAL,
+		Key:  key,
+		Val:  val,
 	}
 }
 
 type ObjectNode struct {
+	Type  NodeType
 	Nodes []Node
+	keys  map[string]struct{}
 }
 
-func (array *ObjectNode) String(tabs string) string {
-	return "{}"
+func (array *ObjectNode) NodeType() NodeType {
+	return array.Type
+}
+
+func NewObjectNode() *ObjectNode {
+	return &ObjectNode{
+		Type:  OBJECT,
+		Nodes: make([]Node, 0),
+		keys:  make(map[string]struct{}),
+	}
+}
+
+func (object *ObjectNode) Add(keyval *KeyValNode) bool {
+	if keyval == nil {
+		log.Println("Tried to add nil keylval")
+		return false
+	}
+
+	if _, ok := object.keys[keyval.Key.Literal]; !ok {
+		return false
+	}
+
+	object.Nodes = append(object.Nodes, keyval)
+	object.keys[keyval.Key.Literal] = struct{}{}
+	return true
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"iter"
+	"log"
 
 	"github.com/lastvoidtemplar/json_formatter/internal/ast"
 	"github.com/lastvoidtemplar/json_formatter/internal/token"
@@ -11,68 +12,243 @@ import (
 
 type Parser struct {
 	currToken token.Token
+	peekToken token.Token
 	nextToken func() (token.Token, bool)
 	stopLexer func()
-	errs      []error
+	parserErr *ParserError
 }
 
-func New(lex iter.Seq[token.Token]) *Parser {
+var ErrEmptyLexer = errors.New("the lexer is empty")
+
+func New(lex iter.Seq[token.Token]) (*Parser, error) {
 	next, stop := iter.Pull(lex)
 
-	tok, ok := next()
+	currToken, ok := next()
 
-	if !ok || tok.Type == token.EOF {
-		return nil
+	if !ok || currToken.Type == token.EOF {
+		return nil, ErrEmptyLexer
+	}
+
+	peekToken, ok := next()
+
+	if !ok {
+		peekToken = newEOF()
 	}
 
 	return &Parser{
 		nextToken: next,
 		stopLexer: stop,
-		currToken: tok,
-		errs:      make([]error, 0),
-	}
+		currToken: currToken,
+		peekToken: peekToken,
+		parserErr: nil,
+	}, nil
 }
 
-func (p *Parser) Parse() (ast.Node, []error) {
+func (p *Parser) Parse() (ast.Node, *ParserError) {
 	defer p.stopLexer()
 
-	if isCurrLeaf(p.currToken) {
-		peekToken, ok := p.nextToken()
+	root := p.parseNode()
 
-		if !ok || peekToken.Type == token.EOF {
-			return ast.NewLeafNode(p.currToken), nil
-		}
-
-		if peekToken.Type == token.ERR {
-			p.errs = append(p.errs, errors.New(peekToken.Literal))
-			return ast.NewLeafNode(p.currToken), p.errs
-		}
-
-		p.errs = append(p.errs,
-			fmt.Errorf("row %d colm %d: the expected token was EOF, but got %s", peekToken.Row, peekToken.Colm, peekToken.Literal))
-		return ast.NewLeafNode(p.currToken), p.errs
+	if root == nil {
+		panic("root is nil")
 	}
 
-	if p.currToken.Type == token.LEFT_CURLY {
-		return nil, nil
+	if p.currToken.Type != token.EOF {
+		p.parserErr = newParserErr(fmt.Sprintf("expected EOF on row %d, colm %d but got %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil, p.parserErr
+	}
+
+	if p != nil {
+		return nil, p.parserErr
+	}
+
+	return root, nil
+}
+
+func (p *Parser) parseNode() ast.Node {
+	if isCurrLeaf(p.currToken) {
+		tok := p.parserLeaf()
+		p.NextToken()
+		return tok
 	}
 
 	if p.currToken.Type == token.LEFT_SQUARE {
-		return nil, nil
+		tok := p.parseArray()
+		p.NextToken()
+		return tok
+
 	}
 
-	p.errs = append(
-		p.errs, fmt.Errorf(
-			"row %d colm %d: expected first token was STRING, NUMBER, BOOL, NULL, { or [, but got %s",
-			p.currToken.Row, p.currToken.Colm, p.currToken.Literal))
-	return nil, p.errs
+	if p.currToken.Type == token.LEFT_CURLY {
+		tok := p.parseObject()
+		p.NextToken()
+		return tok
+	}
+
+	return nil
+}
+
+func (p *Parser) parserLeaf() ast.LeafNode {
+	if !isCurrLeaf(p.currToken) {
+		log.Println("Expecred leaf token")
+		p.parserErr = newParserErr(fmt.Sprintf("expected leaf on row %d, colm %d, but got %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	if p.currToken.Type == token.UNDEFINED {
+		p.parserErr = newParserErr(fmt.Sprintf("undefined token on row %d, colm %d - %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	return ast.NewLeafNode(p.currToken)
+}
+
+func (p *Parser) parseArray() *ast.ArrayNode {
+	if p.currToken.Type != token.LEFT_SQUARE {
+		log.Println("The starting token of array is not [")
+		p.parserErr = newParserErr(fmt.Sprintf("expected [ on row %d, colm %d, but got %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	arrNode := ast.NewArrayNode()
+
+	p.NextToken()
+	for p.currToken.Type != token.RIGHT_SQUARE {
+		if p.currToken.Type == token.EOF {
+			p.parserErr = newParserErr(fmt.Sprintf("Expected ] on row %d, colm %d, but got EOF",
+				p.currToken.Row, p.currToken.Colm), p.currToken.Row, p.currToken.Colm)
+			return nil
+		}
+
+		node := p.parseNode()
+
+		if p.parserErr != nil {
+			return nil
+		}
+
+		if node == nil {
+			panic("Couldn`t parse array element")
+		}
+		arrNode.Add(node)
+
+		p.NextToken()
+
+		if p.currToken.Type != token.SEMICOLON && p.currToken.Type != token.RIGHT_SQUARE {
+			p.parserErr = newParserErr(fmt.Sprintf("expected SEMICOLON or ] on row %d, colm%d got %s",
+				p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+			return nil
+		}
+
+		if p.currToken.Type == token.SEMICOLON {
+			p.NextToken()
+		}
+	}
+
+	return arrNode
+}
+
+func (p *Parser) parseObject() *ast.ObjectNode {
+	if p.currToken.Type == token.EOF {
+		p.parserErr = newParserErr(fmt.Sprintf("Expected ] on row %d, colm %d, but got EOF",
+			p.currToken.Row, p.currToken.Colm), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	objNode := ast.NewObjectNode()
+
+	p.NextToken()
+
+	for p.currToken.Type != token.RIGHT_CURLY {
+		if p.currToken.Type == token.EOF {
+			p.parserErr = newParserErr(fmt.Sprintf("Expected ] on row %d, colm %d, but got EOF",
+				p.currToken.Row, p.currToken.Colm), p.currToken.Row, p.currToken.Colm)
+			return nil
+		}
+
+		node := p.parseKeyVal()
+
+		if p.parserErr != nil {
+			return nil
+		}
+
+		if node == nil {
+			panic("Couldn`t parse object element")
+		}
+
+		ok := objNode.Add(node)
+
+		if !ok {
+			p.parserErr = newParserErr(fmt.Sprintf("duplicate key on row %d, colm%d",
+				node.Key.Row, node.Key.Colm), node.Key.Row, node.Key.Colm)
+		}
+
+		p.NextToken()
+
+		if p.currToken.Type != token.SEMICOLON && p.currToken.Type != token.RIGHT_CURLY {
+			p.parserErr = newParserErr(fmt.Sprintf("expected SEMICOLON or ] on row %d, colm%d got %s",
+				p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+			return nil
+		}
+
+		if p.currToken.Type != token.SEMICOLON {
+			p.NextToken()
+		}
+	}
+
+	return objNode
+}
+
+func (p *Parser) parseKeyVal() *ast.KeyValNode {
+	if p.currToken.Type != token.STRING_LITERAL {
+		p.parserErr = newParserErr(fmt.Sprintf("the key must be string on row %d, colm %d, but got %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	key := p.currToken
+	p.NextToken()
+
+	if p.currToken.Type != token.COLON {
+		p.parserErr = newParserErr(fmt.Sprintf("expected COLON on row %d, colm %d, but got %s",
+			p.currToken.Row, p.currToken.Colm, p.currToken.Literal), p.currToken.Row, p.currToken.Colm)
+		return nil
+	}
+
+	p.NextToken()
+
+	val := p.parseNode()
+	if p.parserErr != nil {
+		return nil
+	}
+
+	return ast.NewKeyVal(key, val)
+}
+
+func (p *Parser) NextToken() {
+	p.currToken = p.peekToken
+	tok, ok := p.nextToken()
+
+	if !ok {
+		p.peekToken = newEOF()
+		return
+	}
+
+	p.peekToken = tok
 }
 
 func isCurrLeaf(tok token.Token) bool {
 	switch tok.Type {
-	case token.NULL, token.TRUE, token.FALSE, token.NUMBER_LITERAL, token.STRING_LITERAL:
+	case token.UNDEFINED, token.NULL, token.TRUE, token.FALSE, token.NUMBER_LITERAL, token.STRING_LITERAL:
 		return true
 	default:
 		return false
 	}
+}
+
+func newEOF() token.Token {
+	return token.New(token.EOF, "", -1, -1)
 }
